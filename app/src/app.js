38c586ee9a9c36aac421009d0e6167674073bf61
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-12-15 21:46:32 -0500

    Add ping and reflection generic API endpoints.

--- app/src/app.js
+++ app/src/app.js
@@ -1,10 +1,18 @@
 'use strict'
 let express = require('express')
+let bodyParser = require('body-parser')
 
 let app = express()
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
+})
+app.get('/ping', (req, res) => {
+  res.send('Pong')
+})
+app.post('/reflection', (req, res) => {
+  res.send(req.body)
 })
 
 module.exports = app
--- app/test/test.js
+++ app/test/test.js
@@ -8,8 +8,35 @@ describe('Homepage', () => {
     request(app)
       .get('/')
       .end((err, res) => {
+        assert.equal(err, null)
         assert.equal(200, res.status)
         assert.equal('Hello, world!', res.text)
+        done()
+      })
+  })
+})
+describe('Ping endpoint', () => {
+  it('Should respond to standard ping', (done) => {
+    request(app)
+      .get('/ping')
+      .end((err, res) => {
+        assert.equal(err, null)
+        assert.equal(200, res.status)
+        assert.equal('Pong', res.text)
+        done()
+      })
+  })
+})
+describe('Json reflection', () => {
+  it('Should return identical Json in response as provided by request', (done) => {
+    let body = { greeting: 'Hello, world!' }
+    request(app)
+      .post('/reflection')
+      .send(body)
+      .end((err, res) => {
+        assert.equal(err, null)
+        assert.equal(200, res.status)
+        assert.equal(body.greeting, res.body.greeting)
         done()
       })
   })
