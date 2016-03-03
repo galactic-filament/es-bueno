72b8e9240ffcf27e5aa46158b18674a49ab99583
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-03-02 21:17:52 -0500

    Add web-dev docker-compose service.
    Send appropriate content-type headers in the default and posts routes.

--- app/src/default.js
+++ app/src/default.js
@@ -4,13 +4,13 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.set('Content-type', 'text/plain').send('Hello, world!')
 })
 router.get('/ping', (req, res) => {
-  res.send('Pong')
+  res.set('Content-type', 'text/plain').send('Pong')
 })
 router.post('/reflection', (req, res) => {
-  res.send(req.body)
+  res.set('Content-type', 'application/json').send(req.body)
 })
 
 module.exports = router
--- app/src/posts.js
+++ app/src/posts.js
@@ -13,7 +13,7 @@ let getRouter = (sequelize) => {
   router.post('/posts', (req, res) => {
     Post.create({ body: req.body.body }).then(
       (post) => res.status(HTTPStatus.CREATED).send({ id: post.id })
-    ).catch((err) => res.status(500).send(err.message))
+    ).catch((err) => res.status(HTTPStatus.SERVER_ERROR).send(err.message))
   })
   router.get('/post/:id', (req, res) => {
     Post.findById(req.params.id).then((post) => res.send(post))
