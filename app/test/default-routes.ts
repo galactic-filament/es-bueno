68996ae0aa550a875016ecf9f531b1fddbcc9508
Adrian Parker <adrian.parker@smith.co>
2016-11-30 15:32:39 -0500

    Remove unused default routes JS test, fix default routes TS test.

--- app/test/default-routes.ts
+++ app/test/default-routes.ts
@@ -8,9 +8,36 @@ test("Homepage Should return standard greeting", (t: test.Test) => {
   const url = "/";
   supertest(app)
     .get(url)
-    .end((err: Error, res: supertest.Response) => {
+    .expect(HTTPStatus.OK)
+    .expect("Hello, world!")
+    .end((err: Error) => {
         t.equal(err, null, `GET ${url} err was not null`);
-        t.equal(res.status, HTTPStatus.OK, `GET ${url} response status was OK`);
+        t.end();
+    });
+});
+
+test("Ping endpoint Should respond to standard ping", (t: test.Test) => {
+  const url = "/ping";
+  supertest(app)
+    .get(url)
+    .expect(HTTPStatus.OK)
+    .expect("Pong")
+    .end((err: Error) => {
+        t.equal(err, null, `GET ${url} err was not null`);
+        t.end();
+    });
+});
+
+test("Json reflection Should return identical Json in response as provided by request", (t: test.Test) => {
+  const url = "/reflection";
+  const body = { greeting: "Hello, world!" };
+  supertest(app)
+    .post(url)
+    .send(body)
+    .expect(HTTPStatus.OK)
+    .end((err: Error, res: supertest.Response) => {
+        t.equal(err, null, `POST ${url} err was not null`);
+        t.equal(body.greeting, res.body.greeting);
         t.end();
     });
 });
