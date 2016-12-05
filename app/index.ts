7b855a6b6fbafde51864a62787653e075df8ccd9
Adrian Parker <adrian.parker@smith.co>
2016-12-05 10:31:25 -0500

    Add missing functionality to app index.

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,11 @@
-console.log("Index!");
+import { app } from "./src/app";
+import * as winston from "winston";
+
+const server = app.listen(8080, () => winston.info("Listening on 8080"));
+process.on("SIGTERM", () => {
+    winston.info("Caught SIGTERM, closing server");
+    server.close(() => {
+        winston.info("Server closed, exiting");
+        process.exit(0);
+    })
+});
