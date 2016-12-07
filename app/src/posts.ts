24f19ce1e2ddc5f57fb722dd45d2fa969fd6b945
Adrian Parker <adrian.parker@smith.co>
2016-12-07 11:09:40 -0500

    Switch to async route handlers.

--- app/src/posts.ts
+++ app/src/posts.ts
@@ -5,6 +5,7 @@ import { json, ParsedAsJson } from "body-parser";
 import * as Sequelize from "sequelize";
 import * as HTTPStatus from "http-status";
 import * as winston from "winston";
+const wrap = require("express-async-wrap");
 
 interface Post {
   id?: number;
@@ -22,35 +23,41 @@ export const getRouter = (sequelize: Sequelize.Connection, _: winston.LoggerInst
       body: Sequelize.STRING
   });
 
-  router.post("/posts", json(), (req: Request & ParsedAsJson, res: Response, next: Function) => {
-      Post.create({ body: req.body.body })
-        .then((post) => res.status(HTTPStatus.CREATED).json({ id: post.id }))
-        .catch((err) => next(err));
-  });
-  router.get("/post/:id", (req: Request, res: Response, next: Function) => {
-    Post.findById(req.params["id"])
-      .then((post) => res.json(post.toJSON()))
-      .catch((err) => next(err));
-  });
-  router.delete("/post/:id", (req: Request, res: Response, next: Function) => {
-    Post.findById(req.params["id"])
-      .then((post) => Post.destroy({ where: { id: post.id } }))
-      .then(() => res.json({}))
-      .catch((err) => next(err));
-  });
-  router.put("/post/:id", json(), (req: Request & ParsedAsJson, res: Response, next: Function) => {
-    Post.findById(req.params["id"])
-      .then((post) => {
-        return new Promise<PostInstance>((resolve, reject) => {
-          Post.update({ body: req.body.body }, { where: { id: post.id } })
-            .then(() => resolve(post))
-            .catch(reject);
-        });
-      })
-      .then((post) => Post.findById(post.id))
-      .then((post) => res.json(post.toJSON()))
-      .catch((err) => next(err));
-  });
+  router.post("/posts", json(), wrap(async (req: Request & ParsedAsJson, res: Response, next: Function) => {
+    try {
+      const post = await Post.create({ body: req.body.body });
+      res.status(HTTPStatus.CREATED).json({ id: post.id });
+    } catch (err) {
+      next(err);
+    }
+  }));
+  router.get("/post/:id", wrap(async (req: Request, res: Response, next: Function) => {
+    try {
+      const post = await Post.findById(req.params["id"]);
+      res.json(post.toJSON());
+    } catch (err) {
+      next(err);
+    }
+  }));
+  router.delete("/post/:id", wrap(async (req: Request, res: Response, next: Function) => {
+    try {
+      const post = await Post.findById(req.params["id"]);
+      await Post.destroy({ where: { id: post.id } });
+      res.json({});
+    } catch (err) {
+      next(err);
+    }
+  }));
+  router.put("/post/:id", json(), wrap(async (req: Request & ParsedAsJson, res: Response, next: Function) => {
+    try {
+      let post = await Post.findById(req.params["id"]);
+      await Post.update({ body: req.body.body }, { where: { id: post.id } });
+      post = await Post.findById(post.id);
+      res.json(post.toJSON());
+    } catch (err) {
+      next(err);
+    }
+  }));
 
   return router;
 };
